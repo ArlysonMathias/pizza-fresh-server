cc90e2480e147b9f7daca31474c18a9742065360
Arlyson Mathias <mathiassouza10@outlook.com>
2022-06-30 11:53:01 -0300

    products and updates

---
+++ src/product/product.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create.product.dto';
+import { UpdateProductDto } from './dto/update.product.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('product')
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criar um produto',
+  })
+  create(@Body() dto: CreateProductDto) {
+    return this.productService.create(dto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Listar todos os produtos',
+  })
+  findAll() {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Listar um produto por ID',
+  })
+  findOne(@Param('id') id: string) {
+    return this.productService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um produto por ID',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
+    return this.productService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Deletar um produto',
+  })
+  delete(@Param('id') id: string) {
+    this.productService.delete(id);
+  }
+}
---
+++ src/product/product.service.ts
@@ -0,0 +1,73 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create.product.dto';
+import { UpdateProductDto } from './dto/update.product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductService {
+  products: Product[] = [];
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<Product[]> {
+    return this.prisma.product.findMany();
+  }
+
+  async findById(id: string): Promise<Product> {
+    const record = await this.prisma.product.findUnique({
+      where: { id },
+    });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  async findOne(id: string): Promise<Product> {
+    return this.findById(id);
+  }
+
+  async create(dto: CreateProductDto): Promise<Product> {
+    const data = { ...dto };
+    try {
+      return await this.prisma.product.create({ data });
+    } catch (error) {
+      return this.handleError(error);
+    }
+  }
+
+  async update(id: string, dto: UpdateProductDto): Promise<Product> {
+    await this.findById(id);
+    const data: Partial<Product> = { ...dto };
+
+    return this.prisma.product.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+    await this.prisma.product.delete({
+      where: { id },
+    });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação',
+    );
+  }
+}
